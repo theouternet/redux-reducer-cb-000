afef9cbc7a17a29ffc762bef8743b263b0f37818
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 20:43:17 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function manageFriends(state, action) {
   switch(action.type) {
     case "ADD_FRIEND":
@@ -13,7 +14,18 @@ export function manageFriends(state, action) {
 
       return { friends: filteredFriends };
 
+=======
+export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      let newFriends = state.friends;
+      newFriends.push(action.friend);
+      return {friends: newFriends};
+    case 'REMOVE_FRIEND':
+      return {friends: state.friends.filter(friend => friend.id !== action.id)};
+>>>>>>> 508688c56517ee09040b48272ac93157a95ddf37
     default:
       return state;
   }
+}
 }
