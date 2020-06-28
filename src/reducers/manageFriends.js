1b4b893a5b569ab654440aa04e8958dd25f1a9d9
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 20:42:36 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return {
+        friends: [...state.friends, action.friend]
+      };
+    case "REMOVE_FRIEND":
+      let friends = [...state.friends];
+
+      let filteredFriends = friends.filter(function(friend, index, array) {
+        return friend.id !== action.id;
+      });
+
+      return { friends: filteredFriends };
+
+    default:
+      return state;
+  }
 }
